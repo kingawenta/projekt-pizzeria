27c13d5bdd4ceba5158d57a81e805e16fe015de7
Kinga Wenta <kinga.wenta@gmail.com>
2021-05-15 11:21:30 +0200

    base widget

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,8 @@
 import {select, templates} from '../settings.js';
 import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
+//import DatePicker from './DatePicker.js';
+//import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(container){
@@ -17,7 +19,8 @@ class Booking {
     thisBooking.dom.wrapper = utils.createDOMFromHTML(generatedHTML);
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
-
+    //thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    //thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
 
     bookingContainer.appendChild(thisBooking.dom.wrapper);
   }
@@ -25,6 +28,8 @@ class Booking {
     const thisBooking = this;
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    //thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    //thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
 
     thisBooking.dom.wrapper.addEventListener('updated', function(){
       thisBooking.updateDOM();
